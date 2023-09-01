7e72b41a23074aca933fe0f2171566dc88becd42
[Simon Parisca <simonpariscam@gmail.com>
2023-09-01 14:41:55 -0500

    listo el efecto de quienes somos

---
+++ JS/jquery.js
@@ -0,0 +1,14 @@
+var movingText = document.getElementById('quienesSomos');
+window.onscroll = function() {
+    var scrollPositionTop = window.scrollY;
+    var scrollPositionBot = window.scrollY + window.pageYOffset;
+    var containerTop = movingText.offsetTop;
+    var containerHeight = movingText.offsetHeight
+
+    if (scrollPositionBot > containerTop && scrollPositionTop < containerTop + containerHeight) {
+        movingText.style = "transition: transform 1.5s, ease 1s; transform: translate3d(0px, 0px, 0px)";
+    } else {
+        movingText.style = "transition: transform 1.5s, ease 1s;  transform: translate3d(150px, 0px, 0px)";
+        movingText.style.opacity = "0";
+    }
+};
