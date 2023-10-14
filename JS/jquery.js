db0c1cbf8e77038a31143982c815e54061b183ba
sparisca04 <simonpariscam@gmail.com>
2023-10-14 12:19:39 -0500

    no dio

--- JS/jquery.js
+++ JS/jquery.js
@@ -26,16 +26,16 @@ function leaveSlide(idTitulo, idPc) {
     title.style = 'transform: translate3d(0px, 0px, 0px); transition: transform 500ms ease 0s';
 }
 
-function apareceTexto(idCirculo, idCuadro, imagen){
+function apareceTexto(idCirculo, idCuadro){
     var circulo = document.getElementById(idCirculo);
     var cuadro = document.getElementById(idCuadro);
-    circulo.style = "transform: translate(-80px, -85px) scale(0.6); transition: transform 500ms ease; background-image: url(" + imagen + ")";
+    circulo.style = "transform: translate(-80px, -85px) scale(0.6); transition: transform 500ms ease;";
     cuadro.style.display = "block";
 }
-function desapareceTexto(idCirculo, idCuadro, imagen){
+function desapareceTexto(idCirculo, idCuadro){
     var circulo = document.getElementById(idCirculo);
     var cuadro = document.getElementById(idCuadro);
-    circulo.style = "transform: translate(0px, 0px) scale(1); transition: transform 500ms ease;background-image: url(" + imagen + ")";
+    circulo.style = "transform: translate(0px, 0px) scale(1); transition: transform 500ms ease;";
     cuadro.style.display = "none";
 }
 const slider = tns({
