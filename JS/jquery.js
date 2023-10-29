72d06a665f12c5ae998a1ebc6b332d671977aded
sparisca04 <simonpariscam@gmail.com>
2023-10-28 20:42:32 -0500

    prueba 1 mapa

--- JS/jquery.js
+++ JS/jquery.js
@@ -73,3 +73,8 @@ const slider = tns({
         contenidoS.innerHTML = "Información de Ventas";
     }
 }
+
+function mostrarEnMapa() {
+    var elemento = document.getElementById("oe");
+    elemento.classList.toggle = "hidden";
+}
